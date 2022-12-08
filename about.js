58fdd5bd824e73e15d977e18d353dbc241cefe3e
sameerah01 <32397454+sameerah01@users.noreply.github.com>
2022-12-08 08:32:00 +0530

    Added about.js just to test

---
+++ about.js
@@ -0,0 +1,12 @@
+function About(props){
+    return(
+        <div>
+            <h2>{props.data}</h2>
+            <p>Sourcewiz, a platform for exporters, has raised $3 million in a seed round of funding. Blume Ventures and Alpha Wave Incubation (AWI), which is backed by DisruptAD and managed by Falcon Edge, co-led the funding round, the company said in a statement.
+
+Read more at: https://yourstory.com/2021/10/funding-alert-sourcewiz-raises-3m-blume-alpha-wave-incubation?utm_pageloadtype=scroll</p>
+        </div>
+    )
+}
+
+export default About;
